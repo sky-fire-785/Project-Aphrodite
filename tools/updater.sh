73aad93b807aff58e54e58e71da6c5a42bf510fa
sky-fire-785 <193379386+sky-fire-785@users.noreply.github.com>
2026-03-31 09:45:02 -0700

    Improve extraction process with error handling
    Added error handling for extraction failures by removing extracted files upon success.

--- tools/updater.sh
+++ tools/updater.sh
@@ -132,11 +132,11 @@ else
     curl -#L -o ~/.config/Project-Aphrodite-REF/ref2 "https://github.com/sky-fire-785/Project-Aphrodite/raw/c2b99b3346a94b4c56c561154c70484ff23680e8/KDE%20Theme/fluency.tar.xz" || echo -e "${RED}WARNING: Install Failed${NC}"
     curl -#L -o ~/.config/Project-Aphrodite-REF/ref3 "https://github.com/sky-fire-785/Project-Aphrodite/raw/c2b99b3346a94b4c56c561154c70484ff23680e8/KDE%20Theme/Project-Aphrodite-Icons.tar.gz" || echo -e "${RED}WARNING: Install Failed${NC}"
 
-    tar -xf ~/.config/Project-Aphrodite-REF/ref0 -C ~/.icons --keep-newer-files &&
-    tar -xf ~/.config/Project-Aphrodite-REF/ref1 -C ~/.local/share/plasma/look-and-feel --keep-newer-files &&
-    tar -xf ~/.config/Project-Aphrodite-REF/ref2 -C ~/.local/share/aurorae/themes --keep-newer-files &&
-    tar -xf ~/.config/Project-Aphrodite-REF/ref3 -C ~/.local/share/icons --keep-newer-files &&
- 
+    tar -xf ~/.config/Project-Aphrodite-REF/ref0 -C ~/.icons --keep-newer-files && rm -rf ~/.config/Project-Aphrodite-REF/ref0 || echo -e "${RED}WARNING: Extration Failed. ${NC}"
+    tar -xf ~/.config/Project-Aphrodite-REF/ref1 -C ~/.local/share/plasma/look-and-feel --keep-newer-files && rm -rf ~/.config/Project-Aphrodite-REF/ref1 || echo -e "${RED}WARNING: Extration Failed. ${NC}"
+    tar -xf ~/.config/Project-Aphrodite-REF/ref2 -C ~/.local/share/aurorae/themes --keep-newer-files && rm -rf ~/.config/Project-Aphrodite-REF/ref2 || echo -e "${RED}WARNING: Extration Failed. ${NC}"
+    tar -xf ~/.config/Project-Aphrodite-REF/ref3 -C ~/.local/share/icons --keep-newer-files && rm -rf ~/.config/Project-Aphrodite-REF/ref3 || echo -e "${RED}WARNING: Extration Failed. ${NC}"
+
 fi
 
 #updates KDE shortcuts
